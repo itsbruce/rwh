62f83b8a91502539d1a988ea5a3fdaf331e13155
Bruce Richardson <it.is.bruce@gmail.com>
2015-07-29 22:47:06 +0100

    Chapter 01 1->2

---
+++ chapter01.hs
@@ -0,0 +1,9 @@
+size :: [a] -> Int
+size [] = 0
+size (_:xs) = 1 + size xs
+
+-- mean :: [Int] -> Double
+mean [] = 0
+mean (x:xs) = mean' 1 x xs
+    where mean' n acc [] = acc / n
+          mean' n acc (y:ys) = mean' (n + 1) (acc + y) ys
